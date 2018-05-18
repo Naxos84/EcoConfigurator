631b4c2c1778f92064e373b34064f2aec3184114
Matthias Kolley <mko@clausohm.de>
2018-05-18 15:26:53 +0200

    Added basic implementation to show content of all .eco files after selecting the configs folder.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,27 @@
 package application;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 
@@ -14,34 +29,68 @@ public class Main extends Application {
 	@Override
 	public void start(final Stage primaryStage) {
 		try {
-            final GridPane root = new GridPane();
-            root.setMinSize(800, 600);
-            root.setPadding(new Insets(10, 10, 10, 10));
-            root.setVgap(5);
-            root.setHgap(5);
-            root.setAlignment(Pos.CENTER);
-            final Button b = new Button();
-            b.setText("Increment");
-            b.setLayoutX(10);
-            b.setLayoutY(10);
+            final BorderPane pane = new BorderPane();
+            pane.setPrefSize(800, 600);
+            pane.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+            pane.setPadding(new Insets(0, 10, 10, 10));
+
+            final TabPane center = new TabPane();
 
 
-            final TextArea textArea = new TextArea();
-            textArea.setLayoutX(100);
-            textArea.setLayoutY(50);
-            textArea.setText("0");
-            textArea.setEditable(false);
-
-            b.setOnMouseClicked(event -> {
-                    Integer clickValue = Integer.parseInt(textArea.getText());
-                    clickValue++;
-                    textArea.setText(clickValue.toString());
-            });
-
-            root.add(b, 0, 0);
-            root.add(textArea, 0, 1);
-            final Scene scene = new Scene(root, 800, 600);
+            pane.setCenter(center);
+            final Scene scene = new Scene(pane, 800, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+            final MenuBar menuBar = new MenuBar();
+            final Menu menuFile = new Menu("File");
+            final MenuItem open = new MenuItem("Load Folder");
+
+            open.setOnAction(event -> {
+                final DirectoryChooser fileChooser = new DirectoryChooser();
+                fileChooser.setTitle("Open Eco Config Folder");
+                final File ecoFolder = fileChooser.showDialog(primaryStage);
+                if (ecoFolder == null) {
+                    System.out.println("No file chosen");
+                }
+                final FilenameFilter fileNameFilter = (dir, name) -> name.endsWith("eco");
+                final File[] ecoConfigFiles = ecoFolder.listFiles(fileNameFilter);
+                if (ecoConfigFiles.length == 0) {
+                    final Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information");
+                    alert.setHeaderText(null);
+                    alert.setContentText("The folder you selected did not contain any .eco file");
+                    alert.show();
+                    return;
+                }
+                center.getTabs().clear();
+                for (final File configFile : ecoConfigFiles) {
+                    final Path configFilePath = Paths.get(configFile.getAbsolutePath());
+                    try {
+                    final String fileContent = new String(Files.readAllBytes(configFilePath));
+                        final Tab tab = new Tab();
+                        tab.setText(configFile.getName());
+
+                        final TextArea textArea = new TextArea();
+                        textArea.setText(fileContent);
+                        textArea.setEditable(false);
+
+                        tab.setContent(textArea);
+                        center.getTabs().add(tab);
+                    } catch (final OutOfMemoryError e) {
+                        e.printStackTrace();
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            });
+            final MenuItem exit = new MenuItem("Exit");
+            exit.setOnAction(event -> Platform.exit());
+            menuFile.getItems().addAll(open, exit);
+
+            menuBar.getMenus().addAll(menuFile);
+            pane.setTop(menuBar);
             primaryStage.setTitle("Incrementor");
 			primaryStage.setScene(scene);
 			primaryStage.show();
